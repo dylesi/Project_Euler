0ae171f80286c18b10976475fbaba834dee6f0ec
dylesi <dyles@hotmail.fi>
2024-08-26 14:10:01 +0300

    Add files via upload

---
+++ Project_Euler/Project_Euler/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Euler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int kolmonen = 3;
+            int vitonen = 5;
+            int loppuluku_1 = 0;
+            int loppuluku_2 = 0;
+            List<int> lista = new List<int>();
+
+            foreach(int index in Enumerable.Range(1, 1000))
+            {
+                if (index % kolmonen == 0)
+                {
+                    lista.Add(index);
+                }
+
+                else if (index % vitonen == 0 && !lista.Contains(index))
+                {
+                    lista.Add(index);
+                }
+            }
+
+            int numero = 0;
+
+            for (int i = 0; i < lista.Count; i++)
+            {
+                numero = numero + lista[i];
+            }
+            Console.WriteLine("Luku on: " + numero);
+            Console.ReadLine();
+        }
+    }
+}
+
+
+// testaa jokainen numero yhdestä tuhanteen. Jos lukujen 3 ja 5 jakojäännös on nolla, on luku kolmen tai viiden kerroin.
---
+++ Project_Euler/Project_Euler/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Project_Euler")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("SSKKY")]
+[assembly: AssemblyProduct("Project_Euler")]
+[assembly: AssemblyCopyright("Copyright © SSKKY 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6ab3de40-41a9-471d-9057-86e28cedc805")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
